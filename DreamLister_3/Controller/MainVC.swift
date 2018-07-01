69c2057d0b729748f27ef90c624ecca3475f366e
vriznet <vriz@vriz.net>
2018-07-01 18:58:36 +0900

    Designing MainVC task is finished

---
+++ DreamLister_3/Controller/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DreamLister_3
+//
+//  Created by vriz on 2018. 7. 1..
+//  Copyright © 2018년 vriz. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
